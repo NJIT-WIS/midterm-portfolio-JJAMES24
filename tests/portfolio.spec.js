396b6af82c0380a7e2ca9d5e8eb908e07729bc80
Jarod <jjamesprojects@hotmail.com>
2023-11-05 22:07:12 -0400

    commit

---
+++ tests/portfolio.spec.js
@@ -0,0 +1,25 @@
+// @ts-check
+const { test, expect } = require('@playwright/test');
+
+const portfolioURL='http://localhost:3000';
+
+//Expected costants
+const expectedresumelink = 'https://jjames24.github.io/classresume/';
+
+  
+//Test resume link on homepage.
+test('Resume Links', async ({ page }) => {
+    // Replace with the URL of the webpage you want to test
+    
+    await page.goto(portfolioURL);
+  
+    // Find the first link with the text "resume"
+    const resumeLink = await page.locator('text=resume').nth(0);
+    await resumeLink.click(); 
+    const currentURL = page.url();
+    expect(currentURL).toBe(expectedresumelink);
+    
+  });
+
+
+
